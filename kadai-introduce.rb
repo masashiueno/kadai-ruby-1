6781a1549c9bc3c7c0227801596c55d2daf23fd5
masashi.ueno <masashiueno@outlook.com>
2021-06-16 14:45:05 +0000

    add #

--- kadai-introduce.rb
+++ kadai-introduce.rb
@@ -1,6 +1,6 @@
-myouzi = "上野"
-space = " "
-namae = "真嗣"
-nenrei = 2*2*2*2*2+9
-introduce = myouzi + space+namae +"です。 現在" + nenrei.to_s + "歳です。　よろしくお願いします。"
-puts introduce
+myouzi = "上野"  #苗字
+space = " "		#スペース
+namae = "真嗣"  #名前
+nenrei = 2*2*2*2*2+9  #年齢
+introduce = myouzi + space + namae +"です。 現在" + nenrei.to_s + "歳です。 よろしくお願いします。"  #合体
+puts introduce  #プット
