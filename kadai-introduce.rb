8266aa32f8e0b82a01069ba421d4d45f8b2bfb28
masashi.ueno <masashiueno@outlook.com>
2021-06-18 13:30:21 +0000

    4

--- kadai-introduce.rb
+++ kadai-introduce.rb
@@ -1,5 +1,4 @@
 myouzi = "上野"  #苗字
 namae = "真嗣"  #名前
 nenrei = 41  #年齢
-introduce = myouzi + namae +"です。" + nenrei.to_s + "歳です。"  #合体
-puts introduce  #プット
+puts myouzi + namae + "です。" + nenrei.to_s + "歳です。"  #プット
